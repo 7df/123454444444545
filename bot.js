fd5a8db677a491d0b4ad1502d98c68ccc95f9289
7df <42803044+7df@users.noreply.github.com>
2019-01-17 15:31:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | shieldbot`)
+client.user.setGame(`$help | shieldbot `)
 client.user.setStatus("online")
 });
 
